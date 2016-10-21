c3415fbfddc355799efa8d48aced752737d29b15
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-10-21 16:05:45 +0300

    Mail views changed.

--- views/mail/new-order.php
+++ views/mail/new-order.php
@@ -1,7 +1,22 @@
 <?php
 /**
  * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ *
+ * @var $products
+ * @var $profile \bl\cms\shop\common\components\user\models\Profile
+ * @var $order \bl\cms\cart\models\Order
+ * @var $address \bl\cms\shop\common\components\user\models\UserAddress
  */
+use yii\bootstrap\Html;
+
 ?>
 
-test message
+<h1><?=Yii::t('cart', 'New order.'); ?></h1>
+<p><?= Html::tag('strong', Yii::t('cart', 'Name')) . ': ' . $profile->name;?></p>
+<p><?= Html::tag('strong', Yii::t('cart', 'Surname')) . ': ' . $profile->surname;?></p>
+<p><?= Html::tag('strong', Yii::t('cart', 'Patronymic')) . ': ' . $profile->patronymic;?></p>
+<br>
+<p><?= Html::tag('strong', Yii::t('cart', 'Phone number')) . ': ' . $profile->phone;?></p>
+
+
+<p><?=Yii::t('cart', 'Our managers will contact you as soon as possible'); ?></p>
--- views/mail/order-success.php
+++ views/mail/order-success.php
@@ -1,9 +1,16 @@
 <?php
 /**
  * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ *
+ * @var $products
+ * @var $profile \bl\cms\shop\common\components\user\models\Profile
+ * @var $order \bl\cms\cart\models\Order
+ * @var $address \bl\cms\shop\common\components\user\models\UserAddress
  */
+
 ?>
 <h1><?=Yii::t('cart', 'Your order is accepted.'); ?></h1>
+<p><?= Yii::t('cart', 'Hallo') . ", $profile->name $profile->surname";?></p>
 <p><?=Yii::t('cart', 'Our managers will contact you as soon as possible'); ?></p>
 
 
