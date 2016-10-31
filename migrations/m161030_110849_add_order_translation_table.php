59e1f6ccb21f154c2fcb4a53f19755c369b5622c
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-10-31 11:41:20 +0200

    FIx.

--- migrations/m161030_110849_add_order_translation_table.php
+++ migrations/m161030_110849_add_order_translation_table.php
@@ -6,32 +6,22 @@ class m161030_110849_add_order_translation_table extends Migration
 {
     public function up()
     {
-        $this->createTable('shop_order_status_translation', [
-            'id' =>$this->primaryKey(),
-            'order_status_id' => $this->integer(),
-            'language_id' => $this->integer(),
-            'title' => $this->string(),
-            'description' => $this->string()
-        ]);
+//        $this->createTable('shop_order_status_translation', [
+//            'id' =>$this->primaryKey(),
+//            'order_status_id' => $this->integer(),
+//            'language_id' => $this->integer(),
+//            'title' => $this->string(),
+//            'description' => $this->string()
+//        ]);
+//
+//        $this->dropColumn('shop_order_status', 'title');
+return true;
 
-        $this->dropColumn('shop_order_status', 'title');
-
-        $this->addForeignKey('shop_order_status_translation_status_id:shop_order_status_id',
-            'shop_order_status_translation', 'order_status_id', 'shop_order_status', 'id', 'cascade', 'cascade');
         $this->addForeignKey('shop_order_status_translation_language_id:language_id',
             'shop_order_status_translation', 'language_id', 'language', 'id', 'cascade', 'cascade');
+        $this->addForeignKey('shop_order_status_translation_status_id:shop_order_status_id',
+            'shop_order_status_translation', 'order_status_id', 'shop_order_status', 'id', 'cascade', 'cascade');
 
-        $this->insert('shop_order_status_translation', [
-            'order_status_id' => 1,
-            'language_id' => 1,
-            'title' => 'Incomplete'
-        ]);
-
-        $this->insert('shop_order_status_translation', [
-            'order_status_id' => 2,
-            'language_id' => 1,
-            'title' => 'Confirmed'
-        ]);
     }
 
     public function down()
