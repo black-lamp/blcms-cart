813c92eb3c98bda56c27c7e7d96dfefd18598153
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-11-03 18:42:28 +0200

    Migrations fix.

--- migrations/m161031_094427_add_order_status_translation.php
+++ migrations/m161031_094427_add_order_status_translation.php
@@ -6,15 +6,16 @@ class m161031_094427_add_order_status_translation extends Migration
 {
     public function up()
     {
+        $language = \bl\multilang\entities\Language::find()->one();
         $this->insert('shop_order_status_translation', [
             'order_status_id' => '1',
-            'language_id' => '2',
+            'language_id' => $language->id,
             'title' => 'Incomplete'
         ]);
 
         $this->insert('shop_order_status_translation', [
             'order_status_id' => '2',
-            'language_id' => '2',
+            'language_id' => $language->id,
             'title' => 'Confirmed'
         ]);
     }
