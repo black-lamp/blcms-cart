7af3c711f56d9c9eb5f0cc5e0687013591596107
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-11-14 13:02:28 +0200

    Adds translations.

--- frontend/Module.php
+++ frontend/Module.php
@@ -1,5 +1,6 @@
 <?php
 namespace bl\cms\cart\frontend;
+use Yii;
 
 /**
  * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
@@ -12,5 +13,16 @@ class Module extends \yii\base\Module
     public function init()
     {
         parent::init();
+        $this->registerTranslations();
+    }
+
+    public function registerTranslations()
+    {
+        Yii::$app->i18n->translations['cart'] =
+            Yii::$app->i18n->translations['nova-poshta'] = [
+            'class'          => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => 'en-US',
+            'basePath'       => '@vendor/black-lamp/blcms-cart/frontend/messages',
+        ];
     }
 }
---
+++ frontend/messages/ru/cart.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ *
+ * Translations for partner requests in Shop Module.
+ */
+
+return [
+    'Address' => 'Адрес',
+    'Apartment' => 'Квартира',
+    'apt.' => 'кв.',
+    'Change personal data' => 'Изменить персональные данные',
+    'Clear cart' => 'Очистить корзину',
+    'City' => 'Город',
+    'Count' => 'Количество',
+    'Country' => 'Страна',
+    'Delivery' => 'Доставка',
+    'Delivery method' => 'Способ доставки',
+    'Delivery methods' => 'Способы доставки',
+    'House' => 'Дом',
+    'hse.' => 'д.',
+    'Name' => 'Имя',
+    'New order' => 'Новый заказ',
+    'Our manager will contact you as soon as possible' => 'Наш менеджер свяжется с вами в самое ближайшее время.',
+    'Patronymic' => 'Отчество',
+    'Phone number' => 'Номер телефона',
+    'Photo' => 'Фото',
+    'Post office' => 'Почтовое отделение',
+    'Price' => 'Цена',
+    'Product title' => 'Название товара',
+    'Products list' => 'Список товаров',
+    'Region' => 'Регион',
+    'Remove' => 'Удалить',
+    'Select delivery method' => 'Способ доставки',
+    'st.' => 'ул.',
+    'Street' => 'Улица',
+    'Success order' => 'Заказ успешно оформлен',
+    'Surname' => 'Фамилия',
+    'Title' => 'Название',
+    'Your order is accepted.' => 'Ваш заказ приянт',
+    'Your personal data' => 'Ваши персональные данные',
+    'Zip' => 'Индекс',
+];
