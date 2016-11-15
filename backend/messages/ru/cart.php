7dba92b66da1303f799ce75f34cb73558ccbe48d
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-11-15 23:38:36 +0200

    Adds translations.

---
+++ backend/messages/ru/cart.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author Albert Gainutdinov <xalbert.einsteinx@gmail.com>
+ * Translations for admin panel part Shop Module.
+ *
+ */
+return [
+    'Articulus' => 'Артикул',
+    'by' => 'из',
+    'Count' => 'Количество',
+    'Creating new delivery method' => 'Добавление новое способа доставки',
+    'Creating new order status' => 'Создание нового статуса заказа',
+    'Customer' => 'Заказчик',
+    'Customer data' => 'Данные заказчика',
+    'Date' => 'Дата заказа',
+    'Delivery method' => 'Способ доставки',
+    'Editing delivery method' => 'Редактирование способа доставки',
+    'Editing order status' => 'Редактирование статуса заказа',
+    'etc' => 'итд.',
+    'New orders' => 'Новые заказы',
+    'Number of items' => 'Количество наименований',
+    'Order details' => 'Детали заказа',
+    'Order list' => 'Список заказов',
+    'Order number' => 'Номер заказа',
+    'Payment method' => 'Способ оплаты',
+    'Post office' => 'Почтовое отделение',
+    'Price' => 'Цена',
+    'Showing items from' => 'Показаны записи с',
+    'Sum' => 'Сумма',
+    'to' => 'по',
+    'Total' => 'Всего',
+    'Your order status is changed to' => 'Статус вашего заказа изменён на'
+];
