cdf8fb58aa8197f372ba4dd3fb6bd0988c407b3d
Vyacheslav Nozhenko <vv.nojenko@gmail.com>
2016-11-24 18:03:04 +0200

    Added 'uk' translations

---
+++ frontend/messages/uk/nova-poshta.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Vyacheslav Nozhenko <vv.nojenko@gmail.com>
+ */
+
+return [
+    'City' => 'Місто',
+    'Start typing the name of the city' => 'Почніть вводити назву міста',
+    'Start typing the name of the street' => 'Почніть вводити назву вулиці',
+];
