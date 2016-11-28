a8cabe2185f85c9452a4c36e66521cebf4dc9951
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-11-28 20:07:07 +0200

    Changes Translation module configuration. Removes old module and adds new.

--- frontend/Module.php
+++ frontend/Module.php
@@ -19,7 +19,7 @@ class Module extends \yii\base\Module
     public function registerTranslations()
     {
         Yii::$app->i18n->translations['cart'] =
-            Yii::$app->i18n->translations['nova-poshta'] = [
+            Yii::$app->i18n->translations['cart'] = [
             'class'          => 'yii\i18n\PhpMessageSource',
             'sourceLanguage' => 'en-US',
             'basePath'       => '@vendor/black-lamp/blcms-cart/frontend/messages',
