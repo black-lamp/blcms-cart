2481ce0555c96758f5bfa1168cc4954414639ac7
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2017-02-08 14:45:07 +0200

    Fix.

--- common/components/user/models/Profile.php
+++ common/components/user/models/Profile.php
@@ -50,7 +50,7 @@ class Profile extends BaseProfile
     {
         return [
             [['user_id'], 'integer'],
-            [['phone'], 'string', 'max' => 16],
+            [['phone'], 'string', 'max' => 25],
             [['name', 'surname', 'phone'], 'required'],
             [['name', 'surname', 'patronymic', 'avatar'], 'string', 'max' => 255],
             [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
