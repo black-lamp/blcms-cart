92612cd6c19f450e540f620cc15338c9b8ea342a
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2017-02-08 16:36:45 +0200

    Fix delivery in mail.

--- frontend/views/mail/delivery.php
+++ frontend/views/mail/delivery.php
@@ -8,27 +8,29 @@
 use yii\helpers\Html;
 
 ?>
-<h3><?= Yii::t('cart', 'Delivery'); ?></h3>
+<?php if (!empty($order->deliveryMethod)): ?>
+    <h3><?= Yii::t('cart', 'Delivery'); ?></h3>
 
-<?php if (!empty($order->delivery_post_office)) : ?>
-    <p><?= Html::tag('strong', Yii::t('cart', 'Delivery method')) . ': ' .
-        $order->deliveryMethod->translation->title; ?>
-    </p>
-    <p><?= Html::tag('strong', Yii::t('cart', 'Post office')) . ': ' . $order->delivery_post_office; ?></p>
-<?php else : ?>
-    <p><?= Html::tag('strong', Yii::t('cart', 'Delivery method')) . ': ' .
-        $order->deliveryMethod->translation->title; ?>
-    </p>
-    <p><?= (!empty($address->country)) ?
-            Html::tag('strong', Yii::t('cart', 'Country')) . ': ' . $address->country : ''; ?></p>
-    <p><?= (!empty($address->region)) ?
-            Html::tag('strong', Yii::t('cart', 'Region')) . ': ' . $address->region : ''; ?></p>
-    <p><?= (!empty($address->city)) ?
-            Html::tag('strong', Yii::t('cart', 'City')) . ': ' . $address->city : ''; ?></p>
-    <p><?= (!empty($address->house)) ?
-            Html::tag('strong', Yii::t('cart', 'House')) . ': ' . $address->house : ''; ?></p>
-    <p><?= (!empty($address->apartment)) ?
-            Html::tag('strong', Yii::t('cart', 'Apartment')) . ': ' . $address->apartment : ''; ?></p>
-    <p><?= (!empty($address->zipcode)) ?
-            Html::tag('strong', Yii::t('cart', 'Zip')) . ': ' . $address->zipcode : ''; ?></p>
+    <?php if (!empty($order->delivery_post_office)) : ?>
+        <p><?= Html::tag('strong', Yii::t('cart', 'Delivery method')) . ': ' .
+            $order->deliveryMethod->translation->title; ?>
+        </p>
+        <p><?= Html::tag('strong', Yii::t('cart', 'Post office')) . ': ' . $order->delivery_post_office; ?></p>
+    <?php else : ?>
+        <p><?= Html::tag('strong', Yii::t('cart', 'Delivery method')) . ': ' .
+            $order->deliveryMethod->translation->title; ?>
+        </p>
+        <p><?= (!empty($address->country)) ?
+                Html::tag('strong', Yii::t('cart', 'Country')) . ': ' . $address->country : ''; ?></p>
+        <p><?= (!empty($address->region)) ?
+                Html::tag('strong', Yii::t('cart', 'Region')) . ': ' . $address->region : ''; ?></p>
+        <p><?= (!empty($address->city)) ?
+                Html::tag('strong', Yii::t('cart', 'City')) . ': ' . $address->city : ''; ?></p>
+        <p><?= (!empty($address->house)) ?
+                Html::tag('strong', Yii::t('cart', 'House')) . ': ' . $address->house : ''; ?></p>
+        <p><?= (!empty($address->apartment)) ?
+                Html::tag('strong', Yii::t('cart', 'Apartment')) . ': ' . $address->apartment : ''; ?></p>
+        <p><?= (!empty($address->zipcode)) ?
+                Html::tag('strong', Yii::t('cart', 'Zip')) . ': ' . $address->zipcode : ''; ?></p>
+    <?php endif; ?>
 <?php endif; ?>
